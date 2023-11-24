3dbe332da70c47f9e465e4ef120437c734497ff3
leeyuhyun0104 <dldbgus0104@gmail.com>
2023-11-24 11:21:10 +0900

    2-02

---
+++ src/test/java/com/ll/sbbmission/SbbmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbbmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbmissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
