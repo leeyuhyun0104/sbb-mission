8b90a6e5b626a9a2a5ef826b4ccbf8b6a250bc80
leeyuhyun0104 <dldbgus0104@gmail.com>
2023-11-24 11:21:10 +0900

    2-01

---
+++ src/main/java/com/ll/sbbmission/SbbmissionApplication.java
@@ -0,0 +1,13 @@
+package com.ll.sbbmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbmissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbbmissionApplication.class, args);
+    }
+
+}
