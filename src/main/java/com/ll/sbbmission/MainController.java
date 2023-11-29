c4ca81f67047b158edfab78e42d534c0f03e78d3
leeyuhyun0104 <dldbgus0104@gmail.com>
2023-11-29 16:49:34 +0900

    2-08

--- src/main/java/com/ll/sbbmission/MainController.java
+++ src/main/java/com/ll/sbbmission/MainController.java
@@ -10,6 +10,11 @@ public class MainController {
     @GetMapping("/sbb")
     @ResponseBody
     public String index() {
-        return "index";
+        return "안녕하세요 sbb에 오신것을 환영합니다.";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
     }
 }
