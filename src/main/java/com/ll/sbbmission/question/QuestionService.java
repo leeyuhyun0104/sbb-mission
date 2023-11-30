9ada9ef43fd6f705780e1107bdf88efac0dcbe6a
leeyuhyun0104 <dldbgus0104@gmail.com>
2023-11-30 11:46:41 +0900

    2-10

--- src/main/java/com/ll/sbbmission/question/QuestionService.java
+++ src/main/java/com/ll/sbbmission/question/QuestionService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import com.ll.sbbmission.DataNotFoundException;
 
 @RequiredArgsConstructor
 @Service
@@ -14,4 +16,15 @@ public class QuestionService {
     public List<Question> getList() {
         return this.questionRepository.findAll();
     }
+
+    public Question getQuestion(Integer id){
+        Optional<Question> question = this.questionRepository.findById(id);
+        if(question.isPresent()){
+            return question.get();
+        }
+        else {
+            throw new DataNotFoundException("quesiton not found");
+        }
+    }
+
 }
